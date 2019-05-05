47f3817c48959700e9d480f15dc1e34d73cad750
du Pasquier Noémi <47056691+Nomyra@users.noreply.github.com>
2019-05-05 15:27:59 +0200

    fonction pour rechercher grâce à une chaine de caractères (ne tient pas compte des accents ni de la casse)

--- src/modele/TableItems.java
+++ src/modele/TableItems.java
@@ -1,6 +1,8 @@
 package modele;
 
 import java.io.Serializable;
+import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class TableItems extends Hashtable<String,Item> implements Serializable{
@@ -11,5 +13,17 @@ public class TableItems extends Hashtable<String,Item> implements Serializable{
 	
 	public void ajouter(Item i) {
 		this.put(i.nom, i);
+	}
+	
+	public ArrayList<Item> rechercher(String nom) {
+		ArrayList<Item> liste = new ArrayList<Item>();
+		for (String n : this.keySet()) {
+			String temp = Normalizer.normalize(nom, Normalizer.Form.NFD);
+			nom = temp.replaceAll("[^\\p{ASCII}]", "").toUpperCase();
+			if (n.toUpperCase().contains(nom)) {
+				liste.add(this.get(n));
+			}
+		}
+		return liste;
 	}
 }

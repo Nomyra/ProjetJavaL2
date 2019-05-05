64d62da0a9719684a051b4fbfbe070f33ed6cd82
Cagnasso Marie <marie.cagnasso@gmail.com>
2019-05-05 15:53:23 +0200

    barre de recherche

--- src/controler/ReserveBox.java
+++ src/controler/ReserveBox.java
@@ -3,6 +3,7 @@ package controler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -11,6 +12,7 @@ import modele.Item;
 import modele.Modele;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 
@@ -19,6 +21,10 @@ public class ReserveBox extends VBox {
     private HBox hboxCategorie;
     @FXML
     protected FlowPane reservePane;
+    @FXML
+    private TextField searchField;
+
+    private ArrayList<Item> listItem;
 
     public ReserveBox(Modele m) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/resource/fxml/reserve.fxml"));
@@ -30,6 +36,24 @@ public class ReserveBox extends VBox {
             throw new RuntimeException( e );
         }
         reserve(m);
+
+        //recherche d'item
+        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.equals("")){
+                reservePane.getChildren().removeAll(reservePane.getChildren());
+            }
+            else{
+                reservePane.getChildren().removeAll(reservePane.getChildren());
+                listItem = m.reserve.rechercher(newValue);
+                for (Item item : listItem) {
+                    ImageView iv = new ImageView("resource/images/items/" + item.nom + ".png");
+                    iv.setFitWidth(50);
+                    iv.setFitHeight(50);
+                    iv.setId(item.nom);
+                    reservePane.getChildren().add(iv);
+                }
+            }
+        });
 
     }
     /*----------
